0a21f68b0d2dd95e9bb8ed4563a64341045c52d7
Yaroslav <yrslv.voroshilov@gmail.com>
2024-04-02 09:42:34 +0200

    Remove extra metrics endpoint from ad_publisher

--- ad_bidder/src/ad_bidder/bid/controller.py
+++ ad_bidder/src/ad_bidder/bid/controller.py
@@ -1,8 +1,6 @@
 import logging as log
 
-import prometheus_client as prom
 from fastapi import APIRouter
-from starlette.responses import Response
 
 from ad_bidder.bid import service as bid_service
 from ad_bidder.constant import *
@@ -22,11 +20,3 @@ def post_bid_request(bid_request: BidRequest) -> BidResponse:
 @router.post(AD_BIDDER_BID_NOTICE)
 def post_notice(bid_id: str, imp_id: str, status: int) -> str | None:
     return bid_service.process_notice(bid_id, imp_id, status)
-
-
-@router.get(AD_BIDDER_BID_METRICS)
-def get_metrics():
-    return Response(
-        content=prom.generate_latest(),
-        media_type="text/plain"
-    )
